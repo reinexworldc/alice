3bcf95aab2ff4bf4661671ac5bb737ae19a1850b
reinexworldc <rydanh400@gmail.com>
2026-02-10 15:03:59 +0300

    feat: tool_call while loop

--- cli/main.py
+++ cli/main.py
@@ -1,12 +1,12 @@
-from cli.handlers.command_handler import CommandHandler
-from cli.handlers.context import SessionContext
-from cli.handlers.input_handler import InputHandler
-from cli.handlers.memory_handler import MemoryHandler
-from cli.handlers.stream_handler import StreamHandler
-from cli.handlers.tools_handler import ToolsHandler
-from cli.prompt import PromptSessionController
-from cli.views.parser import ChunkParser
-from cli.commands import CommandsHelper
+from .handlers.command_handler import CommandHandler
+from .handlers.context import SessionContext
+from .handlers.input_handler import InputHandler
+from .handlers.memory_handler import MemoryHandler
+from .handlers.stream_handler import StreamHandler
+from .handlers.tools_handler import ToolsHandler
+from .prompt import PromptSessionController
+from .views.parser import ChunkParser
+from helpers.commands import CommandsHelper
 from core.agent import ChatAgent
 from helpers.prompt.helper import PromptsHelper
 from providers.openai.provider import OpenAIProvider
@@ -50,25 +50,35 @@ def main():
             if message:
                 MemoryHandler.write_user_message(context, message)
 
-                tools = ToolsHandler.load_tools()
-                chunks, raw_chunks = StreamHandler.stream_and_parse(
-                    agent=context.agent,
-                    parser=context.parser,
-                    message=message,
-                    tools=tools,
-                )
+                followup_message = message
+                # Future: separate tool call logic. 
+                while True:
+                    tools = ToolsHandler.load_tools()
+                    chunks, raw_chunks = StreamHandler.stream_and_parse(
+                        agent=context.agent,
+                        parser=context.parser,
+                        message=followup_message,
+                        tools=tools,
+                    )
+                    context.tools_by_index = ToolsHandler.collect_tool_calls(raw_chunks)
 
-                context.tools_by_index = ToolsHandler.collect_tool_calls(raw_chunks)
-                ToolsHandler.execute_tool_calls(context.agent, context.tools_by_index)
+                    if not context.tools_by_index:
+                        break
+
+                    ToolsHandler.execute_tool_calls(
+                        context.agent, context.tools_by_index
+                    )
+
+                    followup_message = "continue"
 
                 llm_message = "".join(chunks)
                 MemoryHandler.write_assistant_message(context, llm_message)
-
             print("")
         except KeyboardInterrupt:
             continue
         except EOFError:
             break
+
 
 if __name__ == "__main__":
     main()
